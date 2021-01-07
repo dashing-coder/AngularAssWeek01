46347af90baf108cc5a955fd5497326fa3df31d2
Abdul Alim <9306924+dashing-coder@users.noreply.github.com>
2021-01-07 19:16:00 +0600

    07 Jan 2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { HtmlCssComponent } from './courses/html-css/html-css.component';
+import { JavascriptComponent } from './courses/javascript/javascript.component';
+import { AngularComponent } from './courses/angular/angular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AboutComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    FooterComponent,
+    HtmlCssComponent,
+    JavascriptComponent,
+    AngularComponent
   ],
   imports: [
     BrowserModule,
